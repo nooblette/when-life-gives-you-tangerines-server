b2718cb66f3c0b7758974059152d864c8fecfc30
nooblette <min9hyuk@gmail.com>
2025-04-11 21:55:42 +0900

    init : 프로젝트 초기 설정

---
+++ src/main/kotlin/com/tangerine/api/WhenLifeGivesYouTangerinesServerApplication.kt
@@ -0,0 +1,11 @@
+package com.tangerine.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhenLifeGivesYouTangerinesServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhenLifeGivesYouTangerinesServerApplication>(*args)
+}
