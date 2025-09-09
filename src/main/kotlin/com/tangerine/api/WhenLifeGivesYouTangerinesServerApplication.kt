c0a07824b01275910020e7f317e80c5f21735faa
nooblette <min9hyuk@gmail.com>
2025-09-09 20:10:25 +0900

    feat : Https 설정 추가 및 SSL 활성화

--- src/main/kotlin/com/tangerine/api/WhenLifeGivesYouTangerinesServerApplication.kt
+++ src/main/kotlin/com/tangerine/api/WhenLifeGivesYouTangerinesServerApplication.kt
@@ -1,12 +1,37 @@
 package com.tangerine.api
 
+import org.apache.catalina.Context
+import org.apache.tomcat.util.descriptor.web.SecurityCollection
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 @EnableFeignClients
-class WhenLifeGivesYouTangerinesServerApplication
+class WhenLifeGivesYouTangerinesServerApplication {
+    // Enable SSL
+    @Bean
+    fun servletContainer(): ServletWebServerFactory =
+        object : TomcatServletWebServerFactory() {
+            override fun postProcessContext(context: Context) {
+                context.requireHttpsForAllPaths()
+            }
+        }
+
+    private fun Context.requireHttpsForAllPaths() {
+        val collection = SecurityCollection().apply { addPattern("/*") }
+        val constraint =
+            SecurityConstraint().apply {
+                userConstraint = "CONFIDENTIAL"
+                addCollection(collection)
+            }
+        addConstraint(constraint)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<WhenLifeGivesYouTangerinesServerApplication>(*args)
